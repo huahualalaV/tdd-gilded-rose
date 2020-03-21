548a33f92cc217af5261bfeb49e306ca69cfce58
Zijie Jiang <zijie.jiang@thoughtworks.com>
2020-03-21 19:31:32 +0800

    init project

---
+++ src/main/java/cn/xpbootcamp/gilded_rose/Product.java
@@ -0,0 +1,13 @@
+package cn.xpbootcamp.gilded_rose;
+
+/**
+ * @ClassName Product
+ * @Description TODO
+ * @Author zijie.jiang
+ * @Date 2020-03-21 19:30
+ * @Version 1.0
+ */
+public class Product {
+	private Integer sellIn;
+	private Integer quality;
+}
